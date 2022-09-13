c012ef701ce5c376e578bcfbc21f33d456af93b7
aromero7764 <aromero7764@gmail.com>
2022-09-13 09:55:15 -0400

    6to Commit

--- src/components/ICardFetch.jsx
+++ src/components/ICardFetch.jsx
@@ -5,41 +5,44 @@ import '../assets/css/articles.css'
 
 const ICardFetch = ({url}) => {
 
-const [loading, setLoading] = useState(false)
+const [loading, setLoading] = useState(true)
 const [resident, setResident] = useState({})
 
 
 
- useEffect(() =>{
-    setLoading(true)
+useEffect(()=>{ 
     axios.get(url)
-    .then(res => {setResident(res.data)
-        setLoading(false)
-     
-    })
+    .then(res => {
+        setResident(res.data)} )
+    .finally(()=> {})
+ }, []) 
 
-}, []) 
 
 
     return (
-
         <div>
-             <article className='card-items' key={resident?.name}>   
-            <img className='img-item' src={resident?.image} alt="foto" />
-            <h2>Name: {resident?.name}</h2>
-            <div className='status'>
-             <p><span className="icon"><i className={resident.status}></i></span>{resident?.status} - {resident?.species}</p>
+      
+        <article className='card-items' key={resident?.name}>   
+       <img className='img-item' src={resident?.image} alt="foto" />
+       <h2>Name: {resident?.name}</h2>
+       <div className='status'>
+        <p><span className="icon"><i className={resident.status}></i></span>{resident?.status} - {resident?.species}</p>
+
+       </div>
     
-            </div>
-         
-                
-            <p>Origin</p>
-            <p>{resident.origin?.name}</p>
-            <p>episodes where appear</p>
-            <p>{resident.episode?.length}</p>
-            </article> 
-        </div>
-    );
+           
+       <p>Origin</p>
+       <p>{resident.origin?.name}</p>
+       <p>episodes where appear</p>
+       <p>{resident.episode?.length}</p>
+       </article> 
+   </div>
+    )
+    
+    
+  
 };
 
 export default ICardFetch;
+
+/*  loading ? <Loading />: content; */
--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -5,23 +5,43 @@ import { useEffect, useState } from "react";
 
 const useFetch = () => {
   const [data, setData] = useState({});
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   
 
 /* render inicial aleatorio */
   useEffect(() => {
-    const fetchData = async () => {
-        setLoading(true)
           const ramdomId = Math.floor(Math.random() * 126 ) +1
-          const res = await axios.get(`https://rickandmortyapi.com/api/location/${ramdomId}`)
-        setData(res.data)
-      setLoading(false)
-    }
-    fetchData()
+          axios.get(`https://rickandmortyapi.com/api/location/${ramdomId}`)
+            .then(res => {
+              setData(res.data)
+              setLoading(false)
+            })
   }, [])
+
+console.log(data)
 
   /* Renderizado dependiendo de lo que venga del buscador */
       const handleClick = (params) => {
+        let newId = params
+        if (params >= 127) {
+          newId = 126
+        }
+              axios.get(`https://rickandmortyapi.com/api/location/${newId}`)
+                .then(res => {
+                  setData(res.data) 
+                  setLoading(false)})
+                
+    };
+
+  return { data, loading, handleClick};
+};
+
+export default useFetch;
+
+
+/* 
+
+ const handleClick = (params) => {
         let newId = params
         if (params >= 127) {
           newId = 126
@@ -35,8 +55,4 @@ const useFetch = () => {
       } 
       fetchData()
     };
-
-  return { data, loading, handleClick};
-};
-
-export default useFetch;
+*/
